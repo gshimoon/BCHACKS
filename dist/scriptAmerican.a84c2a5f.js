fff887d34b2558adf28ae3413362d357c28a2ca1
ufotan <ufotanshin0508@gmail.com>
2024-01-28 11:09:08 -0800

    linked stuff corrctly

--- dist/scriptAmerican.a84c2a5f.js
+++ dist/scriptAmerican.a84c2a5f.js
@@ -142,7 +142,7 @@ document.addEventListener('DOMContentLoaded', function () {
     window.location.href = 'checkout.html';
   }
 });
-},{}],"../../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{}],"../../../../../Users/ufota/AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -167,7 +167,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50248" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64485" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
@@ -311,5 +311,5 @@ function hmrAcceptRun(bundle, id) {
     return true;
   }
 }
-},{}]},{},["../../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js","scriptAmerican.js"], null)
+},{}]},{},["../../../../../Users/ufota/AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js","scriptAmerican.js"], null)
 //# sourceMappingURL=/scriptAmerican.a84c2a5f.js.map
